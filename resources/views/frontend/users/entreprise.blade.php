4dca481ff7b5e3a35c4f5182713c07b63862add9
Alsace7004 <alabibamidele14@gmail.com>
2022-06-27 18:56:20 +0200

    little update

--- resources/views/frontend/users/entreprise.blade.php
+++ resources/views/frontend/users/entreprise.blade.php
@@ -5,7 +5,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Inscription</title>
+    <title>Inscription Entreprise</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="" />
     <meta name="keywords" content="" />
@@ -33,7 +33,7 @@
                                 <ul class="sign-control">
                                     <a style="margin-right:1rem;background-color:grey;padding:0.2rem 0.5rem;color:#fff;border-top-left-radius:5px;border-bottom-left-radius:5px;" href="{{route('UsersLogin')}}" title="">Connexion</a>
                                     <a style="margin-left:1rem;background-color:grey;padding:0.2rem 0.5rem;color:#fff;border-top-right-radius:5px;border-bottom-right-radius:5px;" href="{{route('UsersRegister')}}" title="">Inscription</a>
-                                    <!--<li data-tab="tab-1" class="current" ><a href="{{route('UsersRegister')}}" title="">Inscription</a></li>-->
+                         
                                 </ul>
                                 
                                 {{-- class="sign_in_sec" id="tab-1">--}}
